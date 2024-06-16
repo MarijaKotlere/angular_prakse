28e0ba7ff283501cc917542a26b7411ee204c0aa
Marija Kotlere <kotlerem@gmail.com>
2024-06-16 19:37:16 +0300

    correct_test

--- backend/app.py
+++ backend/app.py
@@ -37,8 +37,7 @@ def delete_comment(comment_id):
     conn.execute('DELETE FROM comments WHERE id = ?', (comment_id,))
     conn.commit()
     conn.close()
-    return comment_id
-    # return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 @app.route('/api/comment/<int:comment_id>', methods=['GET', 'POST'])
 def edit_comment(comment_id):
